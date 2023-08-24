c54dcfd6d50b0d7ada5e1dfd142ae9ed3014fbce
gustavocunha234 <gustavocunha42@gmail.com>
2023-08-24 13:55:42 -0300

    feat: create modules login

---
+++ vendas/src/App.tsx
@@ -0,0 +1,21 @@
+
+import { SafeAreaView, Text} from 'react-native';
+import Login from './modules/login';
+
+
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView>
+      <Login />
+    </SafeAreaView>
+    
+  );
+};
+
+
+
+export default App;
