30f6e68c0855f66b71517b8be31b36b74c384844
gustavocunha234 <gustavocunha42@gmail.com>
2023-08-29 11:26:52 -0300

    feat: insert types in Text

--- vendas/src/shared/components/text/text.style.ts
+++ vendas/src/shared/components/text/text.style.ts
@@ -4,16 +4,17 @@ import styled from "styled-components/native";
 interface ContainerTextProps {
     color?: string;
     fontSize: string;
+    fontFamily: 'Poppins-Bold' | 'Poppins-Light' | 'Poppins-Regular';
 }
 
 
 export const ContainerText = styled.Text<ContainerTextProps>`
 
-
-
-    font-family: poppins-bold;
+    
     font-size: ${(props: any) => props.fontSize };
+    font-family: ${(props: any) => props.fontFamily };
     ${(props:any) => (props.color ? `color: ${props.color}` : '')}
+    
     
 
     
