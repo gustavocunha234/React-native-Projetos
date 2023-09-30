f55bbfc1670bb4a5313ddede09032219ea8d17c4
gustavocunha234 <gustavocunha42@gmail.com>
2023-09-30 20:19:46 -0300

    feat: insert mask input

--- vendas/src/modules/createUser/screens/CreateUser.tsx
+++ vendas/src/modules/createUser/screens/CreateUser.tsx
@@ -10,9 +10,9 @@ const CreateUser = () => {
     return(
         <CreateUserContainer>
             <Input value={createUser.name} onChange={(event) => handleOnChangeInput(event, 'name')} margin='0px 0px 16px 0px' placeholder="Digite" title="Nome completo: " />
-            <Input value={createUser.phone} onChange={(event) => handleOnChangeInput(event, 'phone')} margin='0px 0px 16px 0px' placeholder="Digite" title="Telefone: " />
+            <Input value={createUser.phone} onChange={(event) => handleOnChangeInput(event, 'phone')} margin='0px 0px 16px 0px' placeholder="Digite" title="Telefone: " type="cel-phone" />
             <Input value={createUser.email} onChange={(event) => handleOnChangeInput(event, 'email')} margin='0px 0px 16px 0px' placeholder="Digite" title="Email: " />
-            <Input value={createUser.cpf} onChange={(event) => handleOnChangeInput(event, 'cpf')} margin='0px 0px 16px 0px' placeholder="Digite" title="CPF: " />
+            <Input value={createUser.cpf}  onChange={(event) => handleOnChangeInput(event, 'cpf')} margin='0px 0px 16px 0px' placeholder="Digite" title="CPF: " type="cpf"/>
             <Input value={createUser.password} onChange={(event) => handleOnChangeInput(event, 'password')} margin='0px 0px 16px 0px' placeholder="Digite" title="Senha: " />
             <Input value={createUser.confirmPassword} onChange={(event) => handleOnChangeInput(event, 'confirmPassword')} margin='0px 0px 16px 0px' placeholder="Digite" title="Confirmar senha: " />
             <Button disabled={disabled} onPress={handleCreateUser} loading={loading} margin='0px 0px 32px 0px' title="Criar usuário" />
