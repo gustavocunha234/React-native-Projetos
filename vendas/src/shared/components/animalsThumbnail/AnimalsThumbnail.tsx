31bb0552865e8037449a163dfea68e5d241be58c
gustavocunha234 <gustavocunha42@gmail.com>
2023-11-02 09:40:32 -0300

    feat: feito a rota entre home e detalhes

--- vendas/src/shared/components/animalsThumbnail/AnimalsThumbnail.tsx
+++ vendas/src/shared/components/animalsThumbnail/AnimalsThumbnail.tsx
@@ -1,7 +1,10 @@
+import { useNavigation } from "@react-navigation/native";
 import { AnimalType } from "../../types/AnimalType";
 import Text from "../text/Text";
 import { textTypes } from "../text/textTypes";
-import { AnimalImage, AnimalsThumbnailContainer } from "./animalsThumbnail.style"
+import { AnimalImage, AnimalsThumbnailContainer } from "./animalsThumbnail.style";
+import {AnimalNavigationProp} from "../../../modules/detail/screens/detail";
+import { MenuUrl } from "../../enums/MenuUrl.enum";
 
 interface AnimalThumbailProps {
     animal: AnimalType;
@@ -10,10 +13,17 @@ interface AnimalThumbailProps {
 
 
 const AnimalsThumbail = ({animal}: AnimalThumbailProps) => {
+    const { navigate } = useNavigation<AnimalNavigationProp>();
+
+    const handGoToDetail = () => {
+        navigate(MenuUrl.DETAIL,  {
+            animal,
+        });
+    };
 
 return (
 
- <AnimalsThumbnailContainer>
+ <AnimalsThumbnailContainer onPress={handGoToDetail}>
      <AnimalImage source={{uri: animal.imagem}} />
     <Text type={textTypes.PARAGRAPH_SMALL_REGULAR}>{animal.name}</Text>
 </AnimalsThumbnailContainer>
